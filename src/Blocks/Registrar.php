c5efac28e2e056723e6e6b255f0e922b34fcf2ec
Dave Anastasi <daveanastasi@imarc.com>
2025-06-18 14:19:10 -0400

    Adds features to block registrar for deregistering blocks and getting all registered blocks.

--- src/Blocks/Registrar.php
+++ src/Blocks/Registrar.php
@@ -5,6 +5,7 @@ namespace Imarc\Millyard\Blocks;
 use Imarc\Millyard\Attributes\RegistersBlock;
 use Imarc\Millyard\Concerns\DiscoversClasses;
 use Imarc\Millyard\Services\Container;
+use WP_Block_Type_Registry;
 
 class Registrar
 {
@@ -33,5 +34,19 @@ class Registrar
 
         $block->register();
         do_action('millyard_block_registered', $blockClass);
+    }
+
+    public function deregisterBlocks(array $blockSlugs): void
+    {
+        foreach ($blockSlugs as $blockSlug) {
+            unregister_block_type($blockSlug);
+        }
+    }
+
+    public function getRegisteredBlocks(): array
+    {
+        $registeredBlocks = WP_Block_Type_Registry::get_instance()->get_all_registered();
+
+        return array_keys($registeredBlocks ?: []);
     }
 }
