0c50adb9a2a078678f0f2690c05637b03f23b7f4
Dave Anastasi <daveanastasi@imarc.com>
2025-07-14 15:50:27 -0400

    Adds scaffolding for taxonomies

--- src/Commands/MillyardCommand.php
+++ src/Commands/MillyardCommand.php
@@ -101,6 +101,39 @@ class MillyardCommand extends Command
         $this->line('Post type class file created.');
     }
 
+    /**
+     * @subcommand make-taxonomy
+     */
+    public function makeTaxonomy($args, $assoc_args)
+    {
+        $label = $this->prompt('Singular label');
+        $pluralLabel = $this->prompt('Plural label', $label . 's');
+        $slug = $this->prompt('Slug', Str::slug($label));
+        $class = $this->prompt('Class name', Str::pascal($label));
+
+        $taxonomyDirectory = sprintf('%s/app/Taxonomies', get_template_directory());
+        if (!is_dir($taxonomyDirectory)) {
+            mkdir($taxonomyDirectory, 0755, true);
+        }
+
+        $taxonomyFile = sprintf('%s/%s.php', $taxonomyDirectory, $class);
+        if (file_exists($taxonomyFile)) {
+            $this->error('Taxonomy class file already exists!');
+            return;
+        }
+
+        $postTypeStub = $this->getStub('PostType', [
+            '{{ class }}' => $class,
+            '{{ slug }}' => $slug,
+            '{{ label }}' => $label,
+            '{{ pluralLabel }}' => $pluralLabel,
+        ]);
+
+        file_put_contents($taxonomyFile, $postTypeStub);
+
+        $this->line('Taxonomy class file created.');
+    }
+
     protected function getStub(string $name, array $replacements = []): string
     {
         $stubFile = realpath(sprintf('%s/../stubs/%s.stub', __DIR__, $name));
